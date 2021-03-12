50c733a9bc325500c0889ec90154130fc98adcb8
Pablo E. Colazurdo <pcolazurdo@yahoo.com>
2021-03-12 18:34:49 +0000

    Added new analysis tools for the KJV version

--- run_container.sh
+++ run_container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm -v `pwd`:/tf/bible $HOME/.ssh:/root/.ssh -p 8888:8888 tensorflow/tensorflow:latest-py3-jupyter
+docker run -it --rm -v `pwd`:/tf/bible -v $HOME/.ssh:/root/.ssh -p 8888:8888 tensorflow/tensorflow:latest-py3-jupyter
